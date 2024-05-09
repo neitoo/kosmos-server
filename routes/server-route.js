546ce481e0c12cf30fbbe82efa652e755f4c443b
neitoo <samuraa855@gmail.com>
2024-05-09 18:04:51 +0500

    Добавлены роуты

--- routes/server-route.js
+++ routes/server-route.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import AuthValidator from "../validations/Validate.js";
 import AuthController from "../controllers/auth-controller.js";
+import DataController from "../controllers/data-controller.js";
 
 const router = Router();
 
 router.post("/auth", AuthValidator.signIn, AuthController.signIn);
 router.post("/registration", AuthValidator.signUp, AuthController.signUp);
 
-
+router.post("/a/user",DataController.getInfoUser);
+router.post("/a/create",DataController.setTasks);
+router.post("/a/all-tasks",DataController.getTasks);
+router.post("/a/task-by-id",DataController.getTaskById);
 
 export default router;
