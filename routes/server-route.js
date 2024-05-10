30d40b1961d02cdfffbc189b726aa55109051586
neitoo <samuraa855@gmail.com>
2024-05-10 20:33:54 +0500

    Добавлены роуты на запрос всех юзеров и обновление задачи

--- routes/server-route.js
+++ routes/server-route.js
@@ -9,8 +9,10 @@ router.post("/auth", AuthValidator.signIn, AuthController.signIn);
 router.post("/registration", AuthValidator.signUp, AuthController.signUp);
 
 router.post("/a/user",DataController.getInfoUser);
+router.post("/a/users", DataController.getAllUsers)
 router.post("/a/create",DataController.setTasks);
 router.post("/a/all-tasks",DataController.getTasks);
 router.post("/a/task-by-id",DataController.getTaskById);
+router.post("/a/update-task",DataController.updateTask);
 
 export default router;
