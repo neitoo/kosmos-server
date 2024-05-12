569c7727fb10d4e91c89ab3ee99cbf21deb7170d
neitoo <samuraa855@gmail.com>
2024-05-12 12:06:22 +0500

    Исправлен вывод ошибок

--- utils/ValidatedRequest.js
+++ utils/ValidatedRequest.js
@@ -1,16 +1,11 @@
 import ErrorUtils, { Unprocessable } from "./Errors.js";
 
 export default async (req, res, next, schema) => {
-  try {
-    if (schema) {
-      await schema.validate(req);
-    }
+    try {
+        if (schema) await schema.validate(req);
 
-    return next();
-  } catch ({ path, errors }) {
-    return ErrorUtils.catchError(
-      res,
-      new Unprocessable(JSON.stringify({ path, errors }))
-    );
-  }
+        return next();
+    } catch ({ path, errors }) {
+        return ErrorUtils.catchError(res, new Unprocessable(`${errors[0]}`));
+    }
 };
