02fa6093684d1737129b067d7705e67ca101e618
neitoo <samuraa855@gmail.com>
2024-05-13 11:38:15 +0500

    перенесены переменные в env

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 import pg from "pg";
 
 const pool = new pg.Pool({
-    user: "",
-    password: "",
-    host: "localhost",
-    port: 5432,
-    database: "todokosmos"
+    user: process.env.USER_DB,
+    password: process.env.PASSWORD_DB,
+    host: process.env.HOST_DB,
+    port: process.env.PORT_DB,
+    database: process.env.NAME_DB
 });
 
 export default pool;
